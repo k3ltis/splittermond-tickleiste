74bbb4eeabf4874330c18476c9a624546cf83051
k3ltis <thomklic@gmail.com>
2024-11-05 20:05:31 +0100

    another attempt to read latest version in github actions

--- scripts/check_version.sh
+++ scripts/check_version.sh
@@ -5,16 +5,22 @@ set -o nounset # Makes script fail on accessing unset variable
 set -o pipefail # Makes pipeline commands fail if at least one command in the pipeline failes
 if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi # Helps debugging the script using `TRACE=1 ./script.sh`
 
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+    echo 'Usage: ./check_version.sh current_version last_version
+
+Compares semantic versions and fails if current_version is not greater than last_version.
+
+'
+    exit
+fi
+
 cd "$(dirname "$0")"
 
 main() {
-    # Get the current version from package.json
-    current_version=$(node -p "require('../package.json').version")
-    echo "Current version: $current_version"
+    current_version=$1
+    last_version=$2
 
-    # Get the last released version from Git tags
-    last_version=$(git describe --tags --abbrev=0)
-    echo "Last released version: $last_version"
+    echo "Comparing versions $current_version vs. $last_version"
 
     # Check if the versions are valid semver
     if [ -z "$last_version" ]; then
@@ -23,7 +29,7 @@ main() {
     else
         # Use semver to compare versions
         if [ $(node -p "require('semver').gt('$current_version', '$last_version')") = "false" ]; then
-            echo "Error: Current version ($current_version) is not greater than last version ($last_version)."
+            echo "Check failed. Current version ($current_version) is not greater than last version ($last_version)."
             exit 1  # Fail the script
         fi
     fi
