7d147ab6dd4da31f850a429b1f303691646c0d33
k3ltis <thomklic@gmail.com>
2024-11-05 20:05:31 +0100

    add language related icons

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -10,11 +10,23 @@
 
 <footer class="footer flex items-center border-t-4 border-primary-content p-4 text-neutral">
 	<aside class="flex-1">
-		<p class="hidden sm:flex">Copyright © k3ltis {new Date().getFullYear()} - {__APP_NAME__} v{__APP_VERSION__}</p>
+		<p class="hidden sm:flex">
+			Copyright © k3ltis {new Date().getFullYear()} - {__APP_NAME__} v{__APP_VERSION__}
+		</p>
 		<p class="sm:hidden">v{__APP_VERSION__}</p>
 	</aside>
-	<button onclick={() => toggleLanguage()}>Langauge</button>
-	<button onclick={() => dataPrivacyModal.showModal()}>{$_("data_privacy")}</button>
+	<button onclick={() => toggleLanguage()}>
+		{#if $locale === 'en'}
+			<img width="30em" src="{base}/svg/flag-for-flag-germany-svgrepo-com.svg" alt="German" />
+		{:else}
+			<img
+				width="30em"
+				src="{base}/svg/flag-for-flag-united-kingdom-svgrepo-com.svg"
+				alt="English"
+			/>
+		{/if}
+	</button>
+	<button onclick={() => dataPrivacyModal.showModal()}>{$_('data_privacy')}</button>
 	<nav class="flex-none">
 		<a href="https://github.com/k3ltis/splittermond-tickleiste" target="_blank">
 			<img width="30em" src="{base}/svg/github-142-svgrepo-com.svg" alt="github" />
@@ -24,8 +36,8 @@
 
 <dialog id="dataPrivacyModal" bind:this={dataPrivacyModal} class="modal">
 	<div class="modal-box">
-		<h3 class="text-lg font-bold">{$_("data_privacy_header")}</h3>
-		<p class="py-4">{$_("data_privacy_content")}</p>
+		<h3 class="text-lg font-bold">{$_('data_privacy_header')}</h3>
+		<p class="py-4">{@html $_('data_privacy_content')}</p>
 	</div>
 	<form method="dialog" class="modal-backdrop">
 		<button>close</button>
