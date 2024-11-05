749432325ae3a252d97e7d1d4893214f08debc56
k3ltis <thomklic@gmail.com>
2024-11-05 20:07:36 +0100

    Updates

---
+++ _app/immutable/chunks/paths.KwPFZ0dk.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1xbkph5)==null?void 0:s.base)??"/splittermond-tickleiste";var t;const a=((t=globalThis.__sveltekit_1xbkph5)==null?void 0:t.assets)??e;export{a,e as b};
