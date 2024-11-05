b7e3bc333b53ecbb71d1cbf3fe2c614a0af173de
k3ltis <thomklic@gmail.com>
2024-11-05 20:07:36 +0100

    Updates

---
+++ _app/immutable/chunks/paths.4H2s3uom.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_rjzad0)==null?void 0:s.base)??"/splittermond-tickleiste";var t;const a=((t=globalThis.__sveltekit_rjzad0)==null?void 0:t.assets)??e;export{a,e as b};
--- _app/immutable/entry/start.DF9NX-eG.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DfZ1a5wG.js";export{t as start};
