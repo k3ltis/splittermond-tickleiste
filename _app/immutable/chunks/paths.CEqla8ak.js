732f76d23173bbea9742fbb52ce6bc521b1a17f5
k3ltis <thomklic@gmail.com>
2024-11-05 20:07:36 +0100

    Updates

---
+++ _app/immutable/chunks/paths.CEqla8ak.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1n4zp0b)==null?void 0:s.base)??"/splittermond-tickleiste";var t;const a=((t=globalThis.__sveltekit_1n4zp0b)==null?void 0:t.assets)??e;export{a,e as b};
