691a5b9bf7c910362a32c1512442ee994ddd667d
k3ltis <thomklic@gmail.com>
2024-11-05 20:05:31 +0100

    Add tour. Replace table by div

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -68,9 +68,11 @@
 </script>
 
 <TickSelection bind:this={tickSelection} />
+
 <div class="mx-48 mt-12 overflow-x-auto">
 	<div class="navbar bg-base-100">
-		<div class="flex-1">
+		<!-- Scene Title -->
+		<div id="sceneTitle" class="flex-1">
 			{#if appMode === AppMode.Editing}
 				<input
 					type="text"
@@ -82,87 +84,96 @@
 				{sceneData.name}
 			{/if}
 		</div>
+		<!-- Change Scene Mode Buttons -->
 		<div class="flex-none space-x-2">
-			<button class="btn" class:btn-active={appMode === AppMode.Running} onclick={runScene}>
+			<button id="runSceneBtn" class="btn" class:btn-active={appMode === AppMode.Running} onclick={runScene}>
 				<img width="30em" src="{base}/svg/circle-play-svgrepo-com.svg" alt="download" />
 				Run Scene
 			</button>
-			<button class="btn" class:btn-active={appMode === AppMode.Editing} onclick={editScene}>
+			<button id="editSceneBtn" class="btn" class:btn-active={appMode === AppMode.Editing} onclick={editScene}>
 				<img width="30em" src="{base}/svg/pencil-svgrepo-com.svg" alt="toggle edit" />
 				Edit Scene
 			</button>
 		</div>
 	</div>
-	<table class="table">
-		<thead>
-			<tr>
-				<th>Name</th>
-				<th>Initiative</th>
-				<th></th>
-			</tr>
-		</thead>
-		<tbody>
-			{#if appMode === AppMode.Editing}
-				<tr>
-					<td>
+	<div id="combatantsTable" class="w-full">
+		<!-- Header -->
+		<div class="flex font-bold py-2 px-4 bg-gray-200" class:rounded-t-lg={appMode === AppMode.Editing} class:rounded-lg={appMode === AppMode.Running}>
+			<div class="flex-1">Name</div>
+			<div class="flex-1" id="initiativeColumn">Initiative</div>
+			<div class="w-16"></div> <!-- Spacer for buttons column -->
+		</div>
+	
+		<!-- Combatant Input Fields -->
+		{#if appMode === AppMode.Editing}
+			<div class="flex items-center bg-gray-200 p-2 rounded-b-lg mb-2">
+				<div class="flex-1">
+					<input
+						type="text"
+						placeholder="Name..."
+						class="input input-bordered w-full max-w-xs"
+						bind:value={newCombatant.name}
+						bind:this={combatantNameInput}
+						onkeydown={handleKeyDown}
+					/>
+				</div>
+				<div class="flex-1">
+					<input
+						type="number"
+						placeholder="Initiative..."
+						class="input input-bordered w-full max-w-xs"
+						bind:value={newCombatant.initiative}
+						onkeydown={handleKeyDown}
+					/>
+				</div>
+				<div class="w-16 flex justify-center">
+					<button onclick={() => addCombatant(newCombatant)} class="btn">
+						<img width="30" src="{base}/svg/plus-svgrepo-com.svg" alt="add" />
+					</button>
+				</div>
+			</div>
+		{/if}
+	
+		<!-- Combatant List -->
+		{#each sceneData.combatants as combatant}
+			<div 
+				class="flex items-center p-2 hover:bg-gray-100 cursor-pointer"
+				onclick={() => combatantClicked(combatant)}
+				onkeydown={() => combatantClicked(combatant)}
+				onkeyup={() => combatantClicked(combatant)}
+				role="button"
+				tabindex="0"
+			>
+				<div class="flex-1">
+					{#if appMode === AppMode.Editing}
 						<input
 							type="text"
-							placeholder="Name..."
 							class="input input-bordered w-full max-w-xs"
-							bind:value={newCombatant.name}
-							bind:this={combatantNameInput}
-							onkeydown={handleKeyDown}
+							bind:value={combatant.name}
 						/>
-					</td>
-					<td>
+					{:else}
+						{combatant.name}
+					{/if}
+				</div>
+				<div class="flex-1">
+					{#if appMode === AppMode.Editing}
 						<input
 							type="number"
-							placeholder="Iniative..."
 							class="input input-bordered w-full max-w-xs"
-							bind:value={newCombatant.initiative}
-							onkeydown={handleKeyDown}
+							bind:value={combatant.initiative}
 						/>
-					</td>
-					<td>
-						<button onclick={() => addCombatant(newCombatant)} class="btn">
-							<img width="30em" src="{base}/svg/plus-svgrepo-com.svg" alt="add" />
+					{:else}
+						{combatant.initiative}
+					{/if}
+				</div>
+				<div class="w-16 flex justify-center">
+					{#if appMode === AppMode.Editing}
+						<button class="btn" onclick={() => deleteCombatant(combatant.id)}>
+							<img width="30" src="{base}/svg/trash-svgrepo-com.svg" alt="delete" />
 						</button>
-					</td>
-				</tr>
-			{/if}
-			{#each sceneData.combatants as combatant}
-				<tr class:hover={appMode === AppMode.Running} onclick={() => combatantClicked(combatant)}>
-					<td>
-						{#if appMode === AppMode.Editing}
-							<input
-								type="text"
-								class="input input-bordered w-full max-w-xs"
-								bind:value={combatant.name}
-							/>
-						{:else}
-							{combatant.name}
-						{/if}
-					</td>
-					<td>
-						{#if appMode === AppMode.Editing}
-							<input
-								type="number"
-								class="input input-bordered w-full max-w-xs"
-								bind:value={combatant.initiative}
-							/>
-						{:else}
-							{combatant.initiative}
-						{/if}
-					</td>
-					<td>
-						{#if appMode === AppMode.Editing}
-							<button class="btn" onclick={() => deleteCombatant(combatant.id)}>
-								<img width="30em" src="{base}/svg/trash-svgrepo-com.svg" alt="delete" />
-							</button>
-						{/if}
-					</td>
-				</tr>
-			{/each}
-		</tbody>
-	</table>
+					{/if}
+				</div>
+			</div>
+		{/each}
+	</div>	
 </div>
