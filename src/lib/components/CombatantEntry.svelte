067dbab759ae8aa870233b8525cb4f77724eda69
Foo Bar <pav.foo@icloud.com>
2024-12-07 16:57:59 +0100

    Replace disabled inputs with static html-elements
    - use p-tags instead of input-tags when app is in RUNNING mode
    - refactor component to achieve separation of RUNNING and EDITING logic
    - - favor duplication instead of entangles logic/structure

--- src/lib/components/CombatantEntry.svelte
+++ src/lib/components/CombatantEntry.svelte
@@ -46,100 +46,114 @@
 	}
 </script>
 
-<div
-	class="
-	    grid w-full grid-cols-[98fr_1fr_1fr] rounded-none p-6 focus:outline-none
-		{appMode === 'RUNNING' ? 'cursor-pointer hover:bg-primary-content' : 'cursor-default'}
+{#if appMode === 'RUNNING'}
+	<div
+		class="
+		rounded p-6 hover:bg-primary-content
 		{index >= 1 ? 'border-t-4 border-primary-content' : ''}"
-	onclick={() => combatantClicked(combatant)}
-	onkeydown={() => {}}
-	onkeyup={() => {}}
-	role="button"
-	tabindex="0"
->
-	<div class="col-span-3 grid grid-cols-subgrid items-center gap-2">
-		<div class="relative">
-			<div class="flex flex-col">
-				<input
-					type="text"
-					aria-label={$_('combatant_name')}
-					disabled={appMode === 'RUNNING'}
-					class="
-					input
-					w-full
-					{appMode === 'EDITING' ? 'input-bordered' : 'disabled input-ghost'}
-					my-auto
-					text-3xl
-					"
-					bind:value={combatant.name}
-				/>
-				{#if appMode === 'RUNNING'}
-					<div class="flex flex-row flex-wrap">
-						{#each combatant.conditionStates as conditionState}
-							<button
-								class="badge badge-error badge-outline badge-lg mr-1 self-end"
-								onclick={(event) => onBadgeClicked(combatant, conditionState, event)}
-							>
-								<span class="text-nowrap"
-									>{resolveLabel(conditionState.id)} ({calculateConditionDuration(
-										combatant,
-										conditionState
-									)})</span
-								>
-							</button>
-						{/each}
+	>
+		<div
+			class="grid w-full cursor-pointer grid-cols-[98fr_1fr_1fr] items-center gap-2"
+			onclick={() => combatantClicked(combatant)}
+			onkeydown={() => {}}
+			onkeyup={() => {}}
+			role="button"
+			tabindex="0"
+		>
+			<div class="relative">
+				<div class="flex flex-col">
+					<p aria-label={$_('combatant_name')} class="my-auto w-full ps-4 text-3xl">
+						{combatant.name}
+					</p>
+				</div>
+				{#if combatant === nextActingCombatant}
+					<div class="absolute -left-4 top-1" in:fade={{ duration: 200 }}>
+						<ArrowRight size="32" />
+						<span class="sr-only">{$_('active_combatant')}</span>
 					</div>
 				{/if}
 			</div>
-			{#if combatant === nextActingCombatant && appMode === 'RUNNING'}
-				<div class="absolute -left-4 top-2.5" in:fade={{ duration: 200 }}>
-					<ArrowRight size="32" />
-					<span class="sr-only">{$_('active_combatant')}</span>
-				</div>
-			{/if}
+			<div class="self-start">
+				{#if combatant.combatState === 'Active'}
+					<p
+						aria-label={$_('combatant_initiative')}
+						class="	w-20 px-1 text-center text-3xl"
+						onfocus={(event: FocusEvent) => selectInputText(event)}
+					>
+						{combatant.initiative}
+					</p>
+				{:else}
+					<div in:fade={{ duration: 200 }}>
+						{#if combatant.combatState === 'Waiting'}
+							<Hourglass class="w-20 text-center text-info" size={48} strokeWidth={1} />
+							<span class="sr-only"
+								>{$_('combatant_status', {
+									values: { status: $_('tickselection.tooltip.set_state_waiting') }
+								})}</span
+							>
+						{:else if combatant.combatState === 'Expecting'}
+							<ClockAlert class="w-20 text-center text-info" size={48} strokeWidth={1} />
+							<span class="sr-only"
+								>{$_('combatant_status', {
+									values: { status: $_('tickselection.tooltip.set_state_expecting') }
+								})}</span
+							>
+						{:else if combatant.combatState === 'Dead'}
+							<Skull class="w-20 text-center text-error" size={48} strokeWidth={1} />
+							<span class="sr-only"
+								>{$_('combatant_status', {
+									values: { status: $_('tickselection.tooltip.set_state_dead') }
+								})}</span
+							>
+						{/if}
+					</div>
+				{/if}
+			</div>
+			<div class="w-16 text-center">
+				<div class="min-w-12"></div>
+			</div>
 		</div>
-		<div class="self-start">
-			{#if combatant.combatState === 'Active' || appMode === 'EDITING'}
+		<div class="mt-2 flex flex-row flex-wrap">
+			{#each combatant.conditionStates as conditionState}
+				<button
+					class="badge badge-error badge-outline badge-lg mr-1 self-end"
+					onclick={(event) => onBadgeClicked(combatant, conditionState, event)}
+				>
+					<span class="text-nowrap"
+						>{resolveLabel(conditionState.id)} ({calculateConditionDuration(
+							combatant,
+							conditionState
+						)})</span
+					>
+				</button>
+			{/each}
+		</div>
+	</div>
+{/if}
+
+{#if appMode === 'EDITING'}
+	<div class="p-6 {index >= 1 ? 'border-t-4 border-primary-content' : ''}">
+		<div class="grid w-full grid-cols-[98fr_1fr_1fr] items-center gap-2">
+			<div class="relative">
+				<div class="flex flex-col">
+					<input
+						type="text"
+						aria-label={$_('combatant_name')}
+						class="input input-bordered my-auto w-full text-3xl"
+						bind:value={combatant.name}
+					/>
+				</div>
+			</div>
+			<div class="self-start">
 				<input
 					type="number"
 					aria-label={$_('combatant_initiative')}
-					disabled={appMode === 'RUNNING'}
-					class="
-					{appMode === 'EDITING' ? 'input-bordered' : 'disabled input-ghost'}
-					input w-20 px-1 text-center text-3xl
-					"
+					class="input input-bordered w-20 px-1 text-center text-3xl"
 					onfocus={(event: FocusEvent) => selectInputText(event)}
 					bind:value={combatant.initiative}
 				/>
-			{:else}
-				<div in:fade={{ duration: 200 }}>
-					{#if combatant.combatState === 'Waiting'}
-						<Hourglass class="w-20 text-center text-info" size={48} strokeWidth={1} />
-						<span class="sr-only"
-							>{$_('combatant_status', {
-								values: { status: $_('tickselection.tooltip.set_state_waiting') }
-							})}</span
-						>
-					{:else if combatant.combatState === 'Expecting'}
-						<ClockAlert class="w-20 text-center text-info" size={48} strokeWidth={1} />
-						<span class="sr-only"
-							>{$_('combatant_status', {
-								values: { status: $_('tickselection.tooltip.set_state_expecting') }
-							})}</span
-						>
-					{:else if combatant.combatState === 'Dead'}
-						<Skull class="w-20 text-center text-error" size={48} strokeWidth={1} />
-						<span class="sr-only"
-							>{$_('combatant_status', {
-								values: { status: $_('tickselection.tooltip.set_state_dead') }
-							})}</span
-						>
-					{/if}
-				</div>
-			{/if}
-		</div>
-		<div class="w-16 text-center">
-			{#if appMode === 'EDITING'}
+			</div>
+			<div class="w-16 text-center">
 				<button
 					in:fade={{ duration: 200 }}
 					class="btn btn-outline btn-error"
@@ -148,12 +162,10 @@
 				>
 					<Trash aria-hidden />
 				</button>
-			{:else if appMode === 'RUNNING'}
-				<div class="min-w-12"></div>
-			{/if}
+			</div>
 		</div>
 	</div>
-</div>
+{/if}
 
 <style>
 	input[type='number']::-webkit-outer-spin-button,
@@ -165,15 +177,5 @@
 	input[type='number'] {
 		-moz-appearance: textfield; /* Firefox */
 		appearance: textfield;
-	}
-
-	/* disabled:bg-transparent disabled:border-none disabled:text-base-content disabled:cursor-default */
-	input.disabled {
-		background: var(--bg-transparent);
-		border-color: transparent;
-		color: var(--text-base-content);
-		cursor: default;
-		pointer-events: none;
-		user-select: none;
 	}
 </style>
