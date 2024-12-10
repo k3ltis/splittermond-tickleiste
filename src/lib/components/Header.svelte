c440e7198c43fcb47168b3198865eeb37060fee0
Foo Bar <pav.foo@icloud.com>
2024-12-10 20:02:04 +0100

    Add shadow for menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -47,7 +47,7 @@
 	<div class="flex-none space-x-2">
 		<ThemeToggle />
 		<Tour />
-		<details class="dropdown dropdown-end" bind:this={details}>
+		<details class="dropdown dropdown-end drop-shadow-xl" bind:this={details}>
 			<summary class="btn btn-ghost" aria-label={$_('open_menu')}>
 				<Menu aria-hidden />
 			</summary>
