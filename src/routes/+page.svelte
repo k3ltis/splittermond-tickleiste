2f7bc4db3ad483594bab48c559a2e2780cea0d28
Foo Bar <pav.foo@icloud.com>
2024-12-18 18:26:48 +0100

    Add loading state
    - avoid white flashes when theme was set to dark aka stop burning my iris when working on this app
    - avoid flash of empty state before scene from local storage was loaded

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,15 +7,20 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import '../i18n';
 	import { _ } from 'svelte-i18n';
+	import { base } from '$app/paths';
+	import { fade } from 'svelte/transition';
+
+	let loading = $state(true);
 
 	onMount(() => {
 		// ensure that title of HTML is set in correct language
-		document.title = $_('app_title');
 
 		const _scene = loadSceneFromLocalStorage();
 		if (_scene) {
 			loadScene(_scene);
 		}
+		// hide loading state after everything has been initialized
+		loading = false;
 	});
 
 	$inspect(sceneData);
@@ -25,9 +30,22 @@
 	});
 </script>
 
-<div class="flex min-h-screen flex-col">
+<div>
 	<!-- <Debug /> -->
-	<Header />
-	<Scene />
-	<Footer />
+	{#if loading}
+		<div
+			class="absolute flex h-full w-full flex-col items-center justify-center bg-base-300"
+			out:fade
+		>
+			<img class="max-h-20" src="{base}/logo.png" alt="splittermond logo" />
+			<span class="loading loading-ring loading-lg my-8 text-primary"></span>
+			<p class="sr-only">{$_('loading_scene')}</p>
+		</div>
+	{:else}
+		<div in:fade={{ delay: 200 }} class="flex min-h-screen flex-col">
+			<Header />
+			<Scene />
+			<Footer />
+		</div>
+	{/if}
 </div>
