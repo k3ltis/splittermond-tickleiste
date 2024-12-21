602a0079f8f38decad93b9135d57497e30df412a
Foo Bar <pav.foo@icloud.com>
2024-12-21 17:27:11 +0100

    Adjust layouting and alert message of condition settings

--- src/lib/components/SettingsModal.svelte
+++ src/lib/components/SettingsModal.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import { conditions, LEVEL_NUMBER_TO_STRING } from '$lib/state/condition';
+	import { conditions, LEVEL_NUMBER_TO_STRING, type Condition } from '$lib/state/condition';
 	import {
 		addCustomCondition,
 		deleteCustomCondition,
 		toggleConditionVisibility
 	} from '$lib/state/scene_data.conditions.svelte';
 	import { sceneData, type Combatant } from '$lib/state/scene_data.svelte';
-	import { Plus, Trash, X } from 'lucide-svelte';
+	import { Plus, Trash, X, TriangleAlert } from 'lucide-svelte';
 	import { _ } from 'svelte-i18n';
 
 	const DEFAULT_CUSTOM_CONDITION_MAX_LEVEL = 1;
@@ -14,6 +14,11 @@
 	let customConditionName: string = $state('');
 	let customConditionMaxLevel: number = $state(DEFAULT_CUSTOM_CONDITION_MAX_LEVEL);
 	let isModalOpen: boolean = $state(false);
+	let conditionsInUse: string[] = $derived(
+		sceneData.combatants.flatMap((combatant) =>
+			combatant.conditionStates.map((conditionState) => conditionState.id)
+		)
+	);
 
 	export function show() {
 		settingsModal.showModal();
@@ -35,6 +40,12 @@
 			return combatant.conditionStates.some((c) => {
 				return c.id === conditionId;
 			});
+		});
+	}
+
+	function isAnyConditionInUse(conditions: Condition[]): boolean {
+		return conditions.some((condition) => {
+			return conditionsInUse.includes(condition.id);
 		});
 	}
 
@@ -79,21 +90,31 @@
 	aria-labelledby="settingsDialogTitle"
 	aria-describedby="dialogDesc"
 >
-	<div class="modal-box border-4 border-primary">
-		<!-- Allow closing by clicking the "X" -->
-		<form method="dialog">
-			<button
-				class="btn btn-circle btn-ghost btn-sm absolute right-4 top-4"
-				aria-label={$_('close')}
-			>
-				<X aria-hidden size={24} />
-			</button>
-		</form>
-		<h3 class="mb-6 text-xl font-bold" id="settingsDialogTitle">{$_('settings.title')}</h3>
+	<div class="modal-box relative border-4 border-primary p-2 pt-0 md:p-4 md:pt-0">
+		<div class="sticky top-0 z-10 flex justify-between bg-base-100 py-3">
+			<!-- Allow closing by clicking the "X" -->
+			<h3 class="pl-3 text-xl font-bold" id="settingsDialogTitle">{$_('settings.title')}</h3>
+			<form method="dialog" class="float-end">
+				<button class="btn btn-circle btn-ghost btn-sm" aria-label={$_('close')}>
+					<X aria-hidden size={24} />
+				</button>
+			</form>
+		</div>
 
 		<div class="divider uppercase before:bg-secondary after:bg-secondary">
 			{$_('settings.conditions')}
 		</div>
+
+		{#if isAnyConditionInUse(conditions)}
+			<div
+				role="alert"
+				class="my-3 flex rounded-md border-4 border-warning bg-base-200 p-3 text-left"
+			>
+				<TriangleAlert aria-hidden size={36} class="mr-3 self-center text-warning" />
+
+				<span class="text-lg">{$_('settings.in_use')}</span>
+			</div>
+		{/if}
 		{#each conditions as condition}
 			<div
 				class="my-1 flex flex-row items-center rounded focus-within:bg-base-300 hover:bg-base-300"
@@ -107,6 +128,7 @@
 					class="toggle toggle-success mr-3"
 					checked={isConditionEnabled(condition.id)}
 					onclick={() => onToggleCondition(condition.id)}
+					disabled={isConditionInUse(condition.id)}
 				/>
 			</div>
 		{/each}
@@ -132,38 +154,42 @@
 				{/each}
 			</select>
 
-			<button
-				class="btn btn-primary mr-2"
-				type="submit"
-				class:btn-disabled={customConditionName.length === 0}
+			<button class="btn btn-primary mr-2" type="submit" disabled={customConditionName.length === 0}
 				><Plus strokeWidth={3} aria-hidden /></button
 			>
 		</form>
 
+		{#if isAnyConditionInUse(sceneData.settings.customConditions)}
+			<div
+				role="alert"
+				class="my-3 flex rounded-md border-4 border-warning bg-base-200 p-3 text-left"
+			>
+				<TriangleAlert aria-hidden size={36} class="mr-3 self-center text-warning" />
+
+				<span class="text-lg">{$_('settings.in_use')}</span>
+			</div>
+		{/if}
+
 		{#each sceneData.settings.customConditions as condition (condition.id)}
 			<div
-				class="my-1 flex flex-row items-center rounded focus-within:bg-base-300 hover:bg-base-300"
+				class="my-1 flex flex-row flex-wrap items-center rounded focus-within:bg-base-300 hover:bg-base-300"
 			>
 				<label for={condition.id} class="flex-1 p-3 text-xl hover:cursor-pointer md:text-2xl"
 					>{$_(condition.i18n)}
 					{resolveLevelrange(condition.maxLevel)}
-					<span class="text-sm text-error">
-						{#if isConditionInUse(condition.id)}
-							(in use)
-						{/if}
-					</span></label
-				>
+				</label>
 				<input
 					type="checkbox"
 					id={condition.id}
-					class="toggle toggle-success mr-4"
+					class="toggle toggle-success m-4"
 					checked={isConditionEnabled(condition.id)}
 					onclick={() => onToggleCondition(condition.id)}
+					disabled={isConditionInUse(condition.id)}
 				/>
 				<button
-					class="btn btn-error btn-sm mr-2 w-12"
+					class="btn btn-error btn-sm mr-2 w-14"
 					onclick={() => onDeleteCustomCondition(condition.id)}
-					class:btn-disabled={isConditionInUse(condition.id)}
+					disabled={isConditionInUse(condition.id)}
 				>
 					<Trash aria-hidden />
 				</button>
